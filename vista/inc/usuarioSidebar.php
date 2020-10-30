64078a8479048b52defe935061fd1ab425d84028
Samuel Ramos <mail@samuelramos.net>
2020-10-29 20:26:59 -0600

    Sidebar del usuario logueado
    Menu de opciones

---
+++ vista/inc/usuarioSidebar.php
@@ -0,0 +1,18 @@
+<div class="left-sidebar">
+    <ul class="list-group list-group-flush">
+        <li><label sytle="font-weight: 800;">ORDENES</label></li>
+        <li class="list-group-item">
+            <a href="ordenes.php">Mis ordenes</a>
+            <span class="badge badge-primary badge-pill">1</span>
+        </li>
+        <li class="list-group-item">
+            <a href="listaDeseos.php">Lista de deseos</a>
+            <span class="badge badge-primary badge-pill">2</span>
+        </li>
+            <br>
+            <li><label sytle="font-weight: 800;">CONFIGURACIONES</label></li>
+            <li class="list-group-item"><a href="infoUsuario.php">Información personal</a></li>
+            <li class="list-group-item"><a href="adminDirecciones.php">Administrar direcciones</a></li>
+            <li class="list-group-item"><a href="updatePassword.php">Actualizar contraseña</a></li>
+    </ul>
+</div>
