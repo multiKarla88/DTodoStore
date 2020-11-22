e67de02170ef24447186233d7ac3336a3993c1be
Samuel Ramos <mail@samuelramos.net>
2020-11-21 23:01:58 -0600

    Create headerAdmin.php

---
+++ vista/inc/admin/headerAdmin.php
@@ -0,0 +1,12 @@
+<header class="fixed-top">
+
+  <!-- Carrito y Login-->
+  <?php include '../inc/loginNav.php' ?>
+
+  <!-- Logo y busqueda-->
+  <?php include '../inc/logoNav.php' ?>
+
+  <!-- Menu de navegacion para pantallas amplias entre paginas. Fondo oscuro-->
+  <?php include 'menuNavAdmin.php' ?>
+
+</header>
