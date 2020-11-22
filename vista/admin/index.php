f3094bbc3179d3824f38f37b4378c2aa6d9b299c
Karla Linares <multikarla88@gmail.com>
2020-11-21 19:49:30 -0600

    Creacion de pantalla de inicio de admin

---
+++ vista/admin/index.php
@@ -0,0 +1,49 @@
+<?php
+  session_start();
+  $currentView = "../admin/index.php"
+?>
+<!DOCTYPE html>
+<html class="no-js" lang="es-sv"> 
+
+  <!-- ***** Head ******-->
+  <head>
+    <title>DTodo Online</title>
+    
+    <!--Se define codificado utf-8 y se especifica la escala para dispositivos mobiles-->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    <?php include './../inc/link.php'; ?>
+    <link rel="stylesheet" href="./../css/estilosUsuario.css">
+  </head>
+
+  <body>
+
+    <!-- ***** Navegacion / Nav ******-->
+    <?php include './../inc/header.php'; ?>
+
+    <!-- Contenido de la pagina -->
+    <section class="container">
+      <div class="row">
+        <div class="col-12">
+
+          <div class="user-wrapper">
+
+            <!--Contenido del usuario-->
+            <div class="user-content">
+              <h2 class="page-header">Información importante de administración</h2>
+              <p>Contenido en Construcción....</p>
+            </div>
+
+          </div>  
+        </div>
+      </div>
+
+
+    </section>
+
+    <!--****** Footer ******-->
+    <?php include './../inc/footer.php'; ?>
+
+  </body>
+</html>
