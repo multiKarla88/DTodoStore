ff1e4afda9402b7c2cfba11b4cb3aaf101674fc8
unknown <ragcorp.g@outlook.com>
2020-11-22 07:19:47 -0600

    clase database para controlador admin

--- modelo/configServer.php
+++ modelo/configServer.php
@@ -3,3 +3,37 @@ define("USER", "root");
 define("PASS", "root");
 define("BD", "store");
 define("SERVER", "localhost");
+
+class Database
+{
+    private static $dbHost = "localhost";
+    private static $dbName = "store";
+    private static $dbUsername = "root";
+    private static $dbUserpassword = "root";
+    
+    private static $connection = null;
+    
+    public static function connect()
+    {
+        if(self::$connection == null)
+        {
+            try
+            {
+              self::$connection = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName, self::$dbUsername, self::$dbUserpassword);
+            }
+            catch(PDOException $e)
+            {
+                die($e->getMessage());
+            }
+        }
+        return self::$connection;
+    }
+    
+    public static function disconnect()
+    {
+        self::$connection = null;
+    }
+
+}
+
+?>
