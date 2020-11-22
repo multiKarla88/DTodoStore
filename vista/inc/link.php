ec3adb67a890bb1e33aa33aedd0350b5c65d1f3c
Gabriel Granados <gg18018@ues.edu.sv>
2020-11-21 22:26:40 -0600

    link.ph reestablecido a valores originales

--- vista/inc/link.php
+++ vista/inc/link.php
@@ -12,21 +12,34 @@
 <script src="js/main.js"></script>
 <script src="js/carrito.js"></script> -->
 
-    <!--Importamos JQuery 3.3.1-->
-    <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
-    <!--Importamos Boostrap 4.1.3-->
+    <!--Importamos JQuery 3.3.1. No modificar-->
+<script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+    <!--Importamos Boostrap 4.1.3. No modificar-->
     <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <!--Estilos generales de la pagina-->
+    <!--Estilos generales de la pagina. Modificable-->
     <link rel="stylesheet" href="css/estilo.css">
-    <!--Fuentes de las secciones de la pagina-->
+    <!--Fuentes de las secciones de la pagina. Modificable-->
     <link rel="stylesheet" href="css/fuentes.css">
-    <!--Colores principales de la pagina-->
+    <!--Colores principales de la pagina. Modificable-->
     <link rel="stylesheet" href="css/colores.css">
-    <!--Importamos estilos del carousel-->
+    <!--Importamos estilos del carousel. No modificar-->
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-    <!--Sliders del carousel-->
+    <!--Sliders y fuentes del carousel. No modificar-->
     <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <!--Fuente del carousel-->
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
-    <!--Script del carousel-->
+    <!--Script del carousel. No modificar-->
     <script src="//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    
+     <!-- Bootstrap Core JavaScript -->
+     <script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="//cdn.jsdelivr.net/bootstrap.filestyle/1.1.0/js/bootstrap-filestyle.min.js"></script>
+    <script type="text/javascript" src="https://assets.jumpseller.com/store/bootstrap/themes/299408/main.js?1596826367"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+    <script type="text/javascript">
+      WebFont.load({
+        google: {
+          families: ["Open Sans:300,400,500,600,700,800", "Open Sans:300,400,500,600,700,800", "Open Sans:300,400,500,600,700,800"]
+        }
+      });
+    </script>
