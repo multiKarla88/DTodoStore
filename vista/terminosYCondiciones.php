d3fcb8be27194c1693f66f1b559cc206edab1176
Gabriel Granados <gg18018@ues.edu.sv>
2020-11-22 21:54:12 -0600

    Se agrega la seccion de terminos y condiciones

---
+++ vista/terminosYCondiciones.php
@@ -0,0 +1,71 @@
+<?php
+  session_start();
+  $currentView = "../index.php"
+?>
+<!DOCTYPE html>
+<html class="no-js" lang="es-sv"> 
+
+  <!-- ***** Head ******-->
+  <head>
+    <title>DTodo Online</title>
+    
+    <!--Se define codificado utf-8 y se especifica la escala para dispositivos mobiles-->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <?php include './inc/link.php' ?>
+  </head>
+
+  <body>
+
+    <!-- ***** Navegacion / Nav ******-->
+    <?php include './inc/header.php' ?>
+
+    <!-- Contenido Principal. Este se pudiera generar dinamicamente-->
+
+    <!-- Contenido de la pagina -->
+    <section class="container"” >
+
+        <style>
+          .col-12{
+            padding-top: 50px;
+          }
+        </style>
+        
+        <div class="col-12">
+            <h2 class="page-header">Terminos y condiciones</h2>
+        </div>
+        
+      <!-- Terminos y Condiciones -->
+        <p>La presente Política de Privacidad establece los términos en que D Todo Store usa y protege la información que es proporcionada por sus usuarios al momento de utilizar su sitio web. Esta compañía está comprometida con la seguridad de los datos de sus usuarios. Cuando le pedimos llenar los campos de información personal con la cual usted pueda ser identificado, lo hacemos asegurando que solo se empleará de acuerdo con los términos de este documento. Sin embargo esta Política de Privacidad puede cambiar con el tiempo o ser actualizada por lo que le recomendamos y enfatizamos revisar continuamente esta página para asegurarse que está de acuerdo con dichos cambios.</p>
+        
+        <p><strong>Información que es recogida</strong></p>
+
+        <p class="mb-0">Nuestro sitio web podrá recoger información personal por ejemplo: Nombre,&nbsp; información de contacto como&nbsp; su dirección de correo electrónico y Número de Identificación Tributaria (NIT). Así mismo cuando sea necesario podrá ser requerida información específica para procesar algún pedido o realizar una entrega o facturación.</p>
+
+        <p><strong>Uso de la información recogida</strong></p>
+
+        <p>Nuestro sitio web emplea la información con el fin de proporcionar el mejor servicio posible, particularmente para mantener un registro de usuarios, de pedidos en caso que aplique, y mejorar nuestros productos y servicios. &nbsp;</p>
+        <p>D Todo Store está altamente comprometido para cumplir con el compromiso de mantener su información segura. Usamos un sistema que  asegura que no existira ningún acceso no autorizado.</p>
+
+        <p><strong>Cookies</strong></p>
+
+        <p>Una cookie se refiere a un fichero que es enviado con la finalidad de solicitar permiso para almacenarse en su ordenador, al aceptar dicho fichero se crea y la cookie sirve entonces para tener información respecto al tráfico web, y también facilita las futuras <a href="" target="_blank"></a> recurrente. Otra función que tienen las cookies es que con ellas las web pueden reconocerte individualmente y por tanto brindarte el mejor servicio personalizado de su web.</p><p>Nuestro <a href="index.php" target="_blank">sitio web</a> se reserva el empleo de las cookies en un futuro para poder identificar las páginas que son visitadas y su frecuencia. Esta información podría ser empleada únicamente para análisis estadístico y después la información se elimina de forma permanente. Usted puede eliminar las cookies en cualquier momento desde su ordenador. Sin embargo las cookies ayudan a proporcionar un mejor servicio de los sitios web, estás no dan acceso a información de su ordenador ni de usted, a menos de que usted así lo quiera y la proporcione directamente. Usted puede aceptar o negar el uso de cookies, sin embargo la mayoría de navegadores aceptan cookies automáticamente pues sirve para tener un mejor servicio web. También usted puede cambiar la configuración de su ordenador para declinar las cookies. Si se declinan, por el momento esto no le imposibilita a usted que pueda utilizar nuestros servicios.</p>
+  
+        <p><strong>Enlaces a Terceros</strong></p>
+        
+        <p>Este sitio web pudiera contener enlaces a otros sitios que pudieran ser de su interés. Una vez que usted de clic en estos enlaces y abandone nuestra página, ya no tenemos control sobre al sitio al que es redirigido y por lo tanto no somos responsables de los términos o privacidad ni de la protección de sus datos en esos otros sitios terceros. Dichos sitios están sujetos a sus propias políticas de privacidad por lo cual es recomendable que los consulte para confirmar que usted está de acuerdo con estas.</p>
+        
+        <p><strong>Control de su información personal</strong></p>
+        
+        <p>En cualquier momento usted puede restringir la recopilación o el uso de la información personal que es proporcionada a nuestro sitio web.&nbsp;</p>
+        <p>Esta compañía no venderá, cederá ni distribuirá la información personal que es recopilada sin su consentimiento o previo aviso, salvo que sea requerido por un juez con una orden judicial.</p>
+        <p>D Todo Store Se reserva el derecho de cambiar los términos de la presente Política de Privacidad en cualquier momento.</p>
+
+    </section>
+
+    <!--****** Footer ******-->
+    <?php include './inc/footer.php' ?>
+
+  </body>
+
+</html>
